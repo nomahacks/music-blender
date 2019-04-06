068164406456840d2de6f1e79185867bab360bae
wisehackermonkey <oranbusiness@gmail.com>
2019-04-06 12:27:44 -0700

    added dynamic text to results, section pages.
    - removed evil halvetica
    - fixed url encoding showing up in dynamic text
    - added logic for results page
    - add demo data set

---
+++ assets/js/results_logic.js
@@ -0,0 +1,32 @@
+
+window.addEventListener('load', function() {
+    var app4 = new Vue({
+        el: '#app-4',
+        data: {
+            result_artist:"Bag Raiders",
+            artist1:"Pink",
+            artist2:"Daft punk",
+        }
+    });
+
+    var params = getAllUrlParams();
+    if(!typeof params.artist1 === undefined) {
+        app4.artist1 = params.artist1;
+    }else{
+        app4.artist1 = "First Artist";
+    }
+
+    if(!typeof params.artist2 === undefined) {
+        app4.artist2 = params.artist2;
+    } else {
+        app4.artist2 = "Second Artist";
+    }
+
+    if(!typeof params.result_artist === undefined) {
+        app4.result_artist = params.result_artist;
+    } else {
+        app4.result_artist = "Chromeo";
+    }
+
+    console.log( params.artist1 + ", " +  params.artist2);
+});
